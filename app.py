ee659580c995005d95b7177b42e17ddf1e9257a9
benmrb <ben.marbacher@gmail.com>
2026-05-11 21:55:35 +0200

    Update app.py
    Löschung "01_Dashboard"

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import streamlit as st
-
 pg = st.navigation(
     [
         st.Page("home.py",                title="Home",       default=True),
-        st.Page("pages/01_Dashboard.py",  title="Dashboard"),
         st.Page("pages/03_Prediction.py", title="Prediction"),
     ],
     position="hidden",
